8225f5f8931118bcb46310ff592b165a81b8f311
lugq1990 <[1131298218@qq.com]>
2019-08-29 09:37:09 +0800

    Change binary search implement

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -6,181 +6,201 @@ is for new coming data, if the value is less then the root value, then
 go to left, otherwise go to the right.
 Here includes with search, insert, delete, remove etc. common actions
 implement for binary tree.
+Also for binary search tree, there couldn't be same value
 """
 
 
-class Node(object):
+class Node:
     def __init__(self, value):
         self.value = value
-        self.left = None
-        self.right = None
+        self.leftChild = None
+        self.rightChild = None
+
+    def insert(self, value):
+        """
+        insert value into the node
+        :param value: which value to be inserted
+        :return: self
+        """
+        if value == self.value:
+            raise ValueError("Couldn't insert same value!")
+        elif value < self.value:
+            # go to the left node
+            if self.leftChild:
+                # if left node exists, then recursively add
+                return self.leftChild.insert(value)
+            else:
+                # if the left node is None
+                self.leftChild = Node(value)
+                return True
+        else:
+            # go the the right node
+            if self.rightChild:
+                return self.rightChild.insert(value)
+            else:
+                self.rightChild = Node(value)
+                return True
+
+    def get_min_node(self, node):
+        """
+        as we could get the min value with left child node value
+        :param node: which node to start to find the value
+        :return: min value node
+        """
+        while node.leftChild is not None:
+            node = node.leftChild
+        return node
+
+    def delete(self, value):
+        """
+        delete value from the tree
+        :param value: which value to be removed
+        :return: the node to be removed
+        """
+        if self is None:
+            # nothing in the tree
+            return None
+
+        if value < self.value:
+            # if value less then the current value, then go the left
+            self.leftChild = self.leftChild.delete(value)
+        elif value > self.value:
+            # if value larger then the current value, then go the right
+            self.rightChild = self.rightChild.delete(value)
+        else:
+            # if with just one node on each side
+            if self.leftChild is None:
+                # create a temperate value to store the right node,
+                # change current value to None
+                tmp = self.rightChild
+                self = None
+                return tmp
+            elif self.rightChild is None:
+                tmp = self.leftChild
+                self = None
+                return tmp
+
+            # then current node both left and right is not None
+            # we should get the right side min value for changing the current node
+            tmp = self.get_min_node(self.rightChild)
+            self.value = tmp.value
+            self.rightChild = self.rightChild.delete(tmp.value)
+        return self
+
+    def find(self, value):
+        """
+        find the value in the tree
+        :param value: which value to find
+        :return: True if Found, otherwise False
+        """
+        if value == self.value:
+            return True
+
+        elif value < self.value:
+            # whether the left node exists
+            if self.leftChild:
+                return self.leftChild.find(value)
+            else:
+                return False
+        else:
+            if self.rightChild:
+                return self.rightChild.find(value)
+            else:
+                return True
+
+    # this is just used to print the value in the tree
+    def preorder(self):
+        if self:
+            print(str(self.value), end=' ')
+            if self.leftChild:
+                self.leftChild.preorder()
+            if self.rightChild:
+                self.rightChild.preorder()
+
+    def inorder(self):
+        if self:
+            if self.leftChild:
+                self.leftChild.inorder()
+            print(str(self.value), end=' ')
+            if self.rightChild:
+                self.rightChild.inorder()
+
+    def postorder(self):
+        if self:
+            if self.leftChild:
+                self.leftChild.postorder()
+            if self.rightChild:
+                self.rightChild.postorder()
+            print(str(self.value), end=' ')
 
 
 class BinarySearchTree:
     def __init__(self):
-        self.head = None
+        self.root = None
 
-    def insert(self, value, node=None):
-        """
-        insert value to the tree
-        :param value: which value to be inserted
-        :param node: which node to compare
-        :return: self
-        """
-        if self.head is None:
-            # if the head is None
-            self.head = Node(value)
+    def insert(self, value):
+        if self.root:
+            return self.root.insert(value)
         else:
-            # tree root node isn't None, then we should compare current value
-            # with root value, if the value is smaller than root value, then
-            # we should go left, otherwise should go right
-            if node is None:
-                # if node is None, then just change the node to head node
-                node = self.head
+            # the node doesn't exist
+            self.root = Node(value)
+            return True
 
-            if value < node.value:
-                # then we should go to left
-                if node.left is None:
-                    # if there isn't any node on current left node
-                    # then just create a new node
-                    node.left = Node(value)
-                else:
-                    # left node is not none, then we should compare current value
-                    # with left node recursively
-                    # have to use return!
-                    return self.insert(value, node.left)
-            else:
-                # if value is bigger than current value, then move to right,
-                # even if the value is same with current value
-                if node.right is None:
-                    # right node is just None, create a new one
-                    node.right = Node(value)
-                else:
-                    # right node is not None, go to right node and compare
-                    return self.insert(value, node.right)
+    def delete(self, value):
+        if self.root is not None:
+            return self.root.delete(value)
 
-    def search(self, value, node=None):
-        """
-        search value from the tree, the time complexity is O(h), h is the tree height.
-        :param value: which value to be searched
-        :param node: which node to compare
-        :return: value if found, else return None means not found
-        """
-        if self.head is None:
-            # if root is None, just return None
-            return None
+    def find(self, value):
+        if self.root:
+            return self.root.find(value)
         else:
-            if node is None:
-                # if this is called for head
-                node = self.head
+            # don't find the value
+            return False
 
-            if value < node.value:
-                if node.left is None:
-                    # nothing could be found, return None
-                    return None
-                else:
-                    # recursively to find the left node
-                    return self.search(value, node.left)
-            elif value > node.value:
-                # go to the right node
-                if node.right is None:
-                    # nothing in the tree! just return None
-                    return None
-                else:
-                    return self.search(value, node.left)
-            elif value == node.value:
-                # we found the value, so great
-                return value
-            else:
-                # even if recursively search the tree, we don't find that value
-                return None
+    def preorder(self):
+        if self.root is not None:
+            print()
+            print('preorder: ')
+            self.root.preorder()
 
-    def _find_min_node(self, node):
-        # recursively to get the left node
-        while node.left is not None:
-            node = node.left
-        return node
+    def inorder(self):
+        if self.root is not None:
+            print()
+            print("inorder:")
+            self.root.inorder()
 
-    def remove(self, value, node=None, parent_node=None):
-        """
-        remove satisfied value from the tree, as we will face with 3 cases:
-        1. remove value is just leaf node
-        2. remove value with just one leaf, then just change the removed node to the leaf node
-        3. remove value has two nodes, then we have to find that node's right node's minimum value
-        to change the current node
-        :param value: which value to be removed
-        :param node: which node to find
-        :return: the node to be removed
-        """
-        if self.head is None:
-            # nothing in the tree
-            return None
-        else:
-            if node is None:
-                node = self.head
-
-            if parent_node is None:
-                parent_node = self.head
-
-            if value < node.value:
-                parent_node = node
-                node = node.left
-                return self.remove(value, node, parent_node)
-            elif value > node.value:
-                parent_node = node
-                node = node.right
-                return self.remove(value, node, parent_node)
-            else:
-                # we get the value
-                if node.left is None:
-                    parent_node = node.right
-                elif node.right is None:
-                    parent_node = node.left
-                else:
-                    # both node have left and right node
-                    min_node = self._find_min_node(node)
-                    node.value = min_node.value
-                    min_node = None
-                    parent_node = node
-                return self
-
-    def delete(self):
-        """
-        delete the whole tree
-        :return: None
-        """
-        self.head = None
+    def postorder(self):
+        if self.root is not None:
+            print()
+            print("Postorder:")
+            self.root.postorder()
 
     def print_var(self, node=None):
-        """
-        print the values in tree to console
-        :param node: which node value to print
-        :return: Nothing
-        """
         if node is None:
-            node = self.head
-            print("Root:", node.value)
+            node = self.root
+            print('root node')
 
-        if node.left is not None:
-            print("Left node value:", node.left.value)
-            self.print_var(node.left)
-        if node.right is not None:
-            print("Right node value: ", node.right.value)
-            self.print_var(node.right)
+        print(node.value)
+        if node.leftChild is not None:
+            print("Left node:")
+            self.print_var(node.leftChild)
+        if node.rightChild is not None:
+            print('right node:')
+            self.print_var(node.rightChild)
 
 
 if __name__ == '__main__':
-    tree = BinarySearchTree()
-    tree.insert(10)
-    tree.insert(8)
-    tree.insert(11)
-    tree.insert(0)
-    tree.print_var()
+    bst = BinarySearchTree()
+    bst.insert(10)
+    bst.insert(1)
+    bst.insert(11)
+    bst.insert(2)
 
-    print("Find :", tree.search(0))
+    bst.print_var()
 
-    print('*'*20)
+    print("Find: ", bst.find(3))
 
-    tree.remove(0)
-    tree.print_var()
+    bst.delete(11)
+    print('*'*10)
+    bst.print_var()
 
